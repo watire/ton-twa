b0ee2779f6076959fcc486731d41e882212b8142
dubin <watire@126.com>
2024-04-23 13:47:30 +0800

    update manifest

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 // this manifest is used temporarily for development purposes
-const manifestUrl =
-  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
-
+const manifestUrl = 'https://watire.github.io/ton-twa/tonconnect-manifest.json';
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
 });
